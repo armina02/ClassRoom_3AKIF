74edc96f7b1e32cb8aabc5f65b62a32d271e2cdc
Armina Salcinovic <sal240766@spengergasse.at>
2024-09-30 17:17:47 +0200

    printMethode hinzugefügt

--- ClassRoom.java
+++ ClassRoom.java
@@ -7,51 +7,50 @@ public class ClassRoom
     private boolean beamer;
     private boolean windows;
     //gehört nur mir, deswegen ist das Anlegen der Eigenschaften privat
-    
+
     public ClassRoom(String neuRoomNr, boolean neuComputer, int neuSize)
     {
         setRoomNr(neuRoomNr);
         setComputer(neuComputer);
         setSize(neuSize);
     }
-    
-    
+
     public void setRoomNr(String neuRoomNr)
     {
         roomNr = neuRoomNr;
     }
-    
+
     public void setComputer(boolean neuComputer)
     {
         computer = neuComputer;
     }
-    
+
     public void setSize(int neuSize)
     {
         size = neuSize;
     }
-    
+
     public String getRoomNr()
     {
         return roomNr;
     }
-    
+
     public boolean getComputer()
     {
         return computer;
     }
-    
+
     public int getSize()
     {
         return size;
     }
-    
-    
-    
-    
-    
+
+    public void printClassRoom()
+
+    { 
+        System.out.println("Classroom " + roomNr + " Computer: " + computer + " - " + size + " Students");
+    }
+
 
     
-    
-  
 }
